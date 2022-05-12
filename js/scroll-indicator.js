91c1cc6d810439450b6f2d4c3066d9d88e9bdb40
Timur <s80516@beuth-hochschule.de>
2022-05-12 13:00:49 +0200

    implement custom slidebar fix back top top button fix responsive design

---
+++ js/scroll-indicator.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+    myFunction()
+};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 4270;
+    document.getElementById("myBar").style.marginTop = scrolled + "%";
+}
