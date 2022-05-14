d925462318fcd3e7e4f3d55d63ed8c0bbe51686f
Timur <s80516@beuth-hochschule.de>
2022-05-14 14:29:00 +0200

    fix responsive design scroll animation

--- js/scroll-indicator.js
+++ js/scroll-indicator.js
@@ -1,10 +1,13 @@
 window.onscroll = function () {
-    myFunction()
+    scrollbarFunction()
+
 };
 
-function myFunction() {
+function scrollbarFunction() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 4270;
-    document.getElementById("myBar").style.marginTop = scrolled + "%";
+    var scrolled = (winScroll / height) * 100;
+    var newscrolled = (scrolled - (scrolled / 100 * 10));
+    console.log(newscrolled);
+    document.getElementById("myBar").style.marginTop = (newscrolled + "vh");
 }
