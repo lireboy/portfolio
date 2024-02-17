8bebe3e2662e8975e673eb5e519eed40d77b23e3
Timur Oz <timur.oez@web.de>
2024-02-17 01:22:02 +0100

    - add final missing alt tag

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -162,7 +162,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="gradient color for the background"></img>
     </section>
   )
 }
