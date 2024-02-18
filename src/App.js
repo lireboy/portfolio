3f4ecd7e31db4e9c60ec105960de6e87f3001c37
Timur Oz <timur.oez@web.de>
2024-02-18 19:26:34 +0100

    - add matomo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/timurportfolio.matomo.cloud/container_wt9f6k5G.js'; s.parentNode.insertBefore(g,s);
+   }, [])
   return (
     <div className="App">
       <NavBar />
